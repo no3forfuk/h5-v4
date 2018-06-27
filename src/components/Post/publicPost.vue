7368d30efeb0c7a6598329a61ecb693de5ecf0a3
Jsir <no3forfuk@163.com>
2018-06-27 19:46:55 +0800

    Signed-off-by: Jsir <no3forfuk@163.com>

--- src/components/Post/publicPost.vue
+++ src/components/Post/publicPost.vue
@@ -1,20 +1,35 @@
 <template>
     <div class="public-post">
+        <input type="file"
+               ref="imgFile"
+               @change="viewImg"
+               style="width: 0px;height:0px;overflow: hidden;position: absolute;"
+               id="inputImg">
+        <input type="file"
+               ref="videoFile"
+               @change="viewVideo"
+               style="width: 0px;height:0px;overflow: hidden;position: absolute;"
+               id="inputVideo">
         <div class="top">
             <rcm-header>
-                <span slot="back">取消</span>
-                <span slot="right">完成</span>
+                <span slot="back" @click="cancelPublic">取消</span>
+                <span slot="right" @click="confirmPublic">完成</span>
             </rcm-header>
             <p>@元素名称</p>
         </div>
         <div class="center">
-            <div contenteditable="true" class="edit-box" @focus="getEditBoxFocus" @blur="lostEditBoxFocus"></div>
+            <div contenteditable="true" class="edit-box"
+                 v-html="postContent"
+                 @click="saveFocusPosition"
+                 @focus="getEditBoxFocus"
+                 @blur="lostEditBoxFocus"></div>
         </div>
         <div class="footer">
-            <icon :value="'&#xe649;'" @click="insertText" style="font-size: 40px"></icon>
-            <icon :value="'&#xe615;'"></icon>
-            <icon :value="'&#xe603;'"></icon>
-            <icon :value="'&#xe628;'"></icon>
+            <icon :value="'&#xe649;'" @click="insertText" class="font-size-28"></icon>
+            <icon :value="'&#xe615;'" @click="insertImg" class="font-size-28"></icon>
+            <icon :value="'&#xe603;'" @click="insertVideo" class="font-size-28"></icon>
+            <icon :value="'&#xe628;'" class="font-size-28"></icon>
+
         </div>
     </div>
 </template>
@@ -24,7 +39,13 @@
     export default {
         data() {
             return {
-                lastEditRange: {}
+                lastEditRange: {},
+                postContent: '',
+                imgNum: 0,
+                imgOldHtml: '',
+                imgNewHtml: '',
+                videoNum: 0,
+                videoNewHtml: ''
             }
         },
         mounted() {
@@ -33,33 +54,80 @@
             })
         },
         methods: {
+            cancelPublic() {
+
+            },
+            confirmPublic() {
+                console.log($('.edit-box').html());
+            },
+            getRangePosition(element) {
+
+            },
+            insertVideo() {
+                this.$refs.videoFile.click()
+            },
+            viewVideo() {
+                const flies = new FileReader();
+                flies.onload = data => {
+                    this.videoNum++
+                    this.videoNewHtml = `
+                      <video src="" id="video${this.videoNum}" controls="controls" style="max-width:300px;"></video>
+                `
+                    $('.edit-box').append(this.videoNewHtml)
+                    $(`#video${this.videoNum}`)[0].src = data.target.result
+                    this.$refs.videoFile.value = ''
+                }
+                if (this.$refs.videoFile.files[0]) {
+                    flies.readAsDataURL(this.$refs.videoFile.files[0])
+                } else {
+                    return
+                }
+            },
             insertText() {
-                let selection = window.getSelection();
-                selection.removeAllRanges()
-                selection.addRange(this.lastEditRange)
-                const range = this.lastEditRange
-                alert(range)
-                $('.edit-box').focus()
-                var textNode = range.startContainer;
-                var rangeStartOffset = range.startOffset;
-                textNode.insertData(rangeStartOffset, '\r\n')
-                console.log(textNode);
+
+            },
+            insertImg() {
+                this.$refs.imgFile.click()
+            },
+            viewImg() {
+                const flies = new FileReader();
+                flies.onload = data => {
+                    this.imgNum++
+                    this.imgNewHtml = `
+                      <br>
+                      <img src="" alt="" style="max-width:300px;margin: 0 auto;border-radius: 4px;" id="img${this.imgNum}">
+                `
+                    $('.edit-box').append(this.imgNewHtml)
+                    $(`#img${this.imgNum}`)[0].src = data.target.result
+                    this.$refs.imgFile.value = ''
+                }
+                if (this.$refs.imgFile.files[0]) {
+                    flies.readAsDataURL(this.$refs.imgFile.files[0])
+                } else {
+                    return
+                }
             },
             getEditBoxFocus() {
+
                 // $('.public-post').height()
                 // setTimeout(() => {
                 //     let height = $('.public-post')[0].clientHeight / 2
                 //     $('.public-post').height(height)
                 // }, 500)
 
-                let selection = window.getSelection();
-                selection.removeAllRanges()
-                selection.addRange(this.lastEditRange)
-
             },
             lostEditBoxFocus() {
-                let selection = window.getSelection();
-                this.lastEditRange = selection.getRangeAt(0)
+                $('.edit-box').on('keyup', null)
+            },
+            saveFocusPosition() {
+                if (window.getSelection) {
+                    let range, selection;
+                    selection = window.getSelection();
+                    this.lastEditRange = selection.getRangeAt(0);
+                    $('.edit-box').on('keyup', () => {
+                        this.lastEditRange = selection.getRangeAt(0);
+                    })
+                }
             }
         },
         watch: {}
@@ -78,6 +146,9 @@
             .edit-box {
                 width: 100%;
                 height: 170px;
+                text-align: left;
+                overflow-x: hidden;
+                overflow-y: auto;
             }
         }
     }
@@ -86,9 +157,9 @@
         width: 100%;
         height: 40px;
         background-color: #ccc;
-        position: absolute;
-        bottom: 0;
-        left: 0;
+        /*position: absolute;*/
+        /*bottom: 0;*/
+        /*left: 0;*/
         i {
             padding: 0 5px;
         }
--- src/main.js
+++ src/main.js
@@ -18,7 +18,20 @@ const router = new VueRouter(routerConfig);
 router.beforeEach((to, from, next) => {
     next()
 })
-
+//todo 优化
+//1.个人主页下方滚动
+//2.编辑个人信息页面之修改擅长领域
+//3.我的消息 完善
+//4.列表下拉刷新
+//5.榜单页面头部发现数据绑定
+//6.添加榜单及元素
+//7.最新，最热排序
+//8.时间过滤器
+//9.点赞动画
+//10.POST评论
+//11.V2-API集成
+//12.接口
+//13.登陆，注册
 //Mint-ui
 import 'mint-ui/lib/style.css'
 import Mint from 'mint-ui';
